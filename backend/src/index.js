6d5a85a052a0c58dda0a2f0c3e596e29ac17b235
aman-sharma14 <sharma.aman.14@proton.me>
2025-04-05 18:44:00 +0530

    auth endpoints defined with cloudinary setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -9,7 +11,9 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json())
+app.use(cookieParser())
 app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 app.listen(PORT,()=>{
     console.log("server is running on port "+PORT);
