253e76d053631d48f80a82d0f48e00ae178c4e65
aman-sharma14 <sharma.aman.14@proton.me>
2025-04-05 22:30:43 +0530

    messages functionality defined

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,27 +1,32 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
 
+
+//this checks if user is authenticated using their cookie to perform othe operations like sending msgs, profile 
+// basically serving pages only if user is loggedin
 export const protectRoute = async (req,res,next)=>{
     try{
 
-        const token = req.cookies.jwt;
+        const token = req.cookies.jwt; //get the jwt cookie 
 
         if(!token){
             return res.status(401).json({message : "Unauthorized - No Token Provided"})
         }
 
-        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        const decoded = jwt.verify(token,process.env.JWT_SECRET) //verify token
 
         if(!decoded) {
             return res.status(401).json({message : "Unauthorized - Invalid token"})
         }
 
-        const user = await User.findById(decoded.userId).select("-password");
+        //extract userId from token and send details except password
+        const user = await User.findById(decoded.userId).select("-password"); 
 
         if(!user){
             return res.status(404).json({message : "User not found"})
         }
 
+        //now add user to request body that will be sent to next operation (eg. profile, msgs, etc)
         req.user = user;
         next();
 
