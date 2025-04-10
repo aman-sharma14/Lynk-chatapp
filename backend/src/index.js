58220d0f7f85e7c2d54e07ef8476dea1eb4a8575
aman-sharma14 <sharma.aman.14@proton.me>
2025-04-10 22:34:27 +0530

    cors changes for production

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,9 +17,13 @@ const __dirname = path.resolve();
 // app.use(express.json())
 app.use(express.json({ limit: '10mb' })); 
 app.use(cookieParser())
+
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true //allows cookies to be sent
+    origin: process.env.NODE_ENV === "development" 
+        ? "http://localhost:5173"  
+        : true,  
+    credentials: true
 }));
 
 
