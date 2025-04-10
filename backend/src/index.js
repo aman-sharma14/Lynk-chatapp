4c410a11b5cb964f5901fa17d2a2900849b0c8bb
aman-sharma14 <sharma.aman.14@proton.me>
2025-04-10 23:10:48 +0530

    path regex error fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,9 +20,7 @@ app.use(cookieParser())
 
 
 app.use(cors({
-    origin: process.env.NODE_ENV === "development" 
-        ? "http://localhost:5173"  
-        : true,  
+    origin: "http://localhost:5173",
     credentials: true
 }));
 
@@ -33,7 +31,7 @@ app.use("/api/messages",messageRoutes)
 if(process.env.NODE_ENV==="production"){
     app.use(express.static(path.join(__dirname,"../frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     });
 }
